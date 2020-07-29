75dc72e9f3b5822fb90a0a2fe281a7c8d384507a
César Gonzalez <cedgo1997@gmail.com>
2020-07-29 17:45:14 -0400

    address fields and validations

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -29,6 +29,14 @@ export class ReactiveComponent implements OnInit {
     return this.forma.get("correo").invalid && this.forma.get("correo").touched;
   }
 
+  get distritoNoValido() {
+    return this.forma.get("direccion.distrito").invalid && this.forma.get("direccion.distrito").touched;
+  }
+
+  get ciudadNoValido() {
+    return this.forma.get("direccion.ciudad").invalid && this.forma.get("direccion.ciudad").touched;
+  }
+
   crearFormulario() {
     this.forma = this.fb.group({
       nombre: ["", [Validators.required, Validators.minLength(5)]],
@@ -40,10 +48,21 @@ export class ReactiveComponent implements OnInit {
           Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$"),
         ],
       ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required],
+        ciudad: ['', Validators.required]
+      })
     });
   }
 
   guardar() {
     console.log(this.forma);
+    if (this.forma.invalid) {
+      Object.values(this.forma.controls).forEach((control) => {
+        control.markAsTouched();
+      });
+    }
+
+    console.log(this.forma.value);
   }
 }
