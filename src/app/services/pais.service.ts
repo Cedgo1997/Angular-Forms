a9e488bedc104e28536c2bec10cf4b9033c88771
César Gonzalez <cedgo1997@gmail.com>
2020-07-28 20:20:12 -0400

    gePais method created

--- src/app/services/pais.service.ts
+++ src/app/services/pais.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getPaises() {
+    return this.http.get('https://restcountries.eu/rest/v2/lang/es');
+  }
+
+
+
+
+
 }
