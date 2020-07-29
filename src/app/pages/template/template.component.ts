1a19b6f8980dd52e662f3f2bdea19009f58bc53f
César Gonzalez <cedgo1997@gmail.com>
2020-07-28 21:09:10 -0400

    set paises variable, and loop into it for options

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,39 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { PaisService } from "src/app/services/pais.service";
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  selector: "app-template",
+  templateUrl: "./template.component.html",
+  styleUrls: ["./template.component.css"],
 })
 export class TemplateComponent implements OnInit {
-
-  constructor() { }
+  constructor(private paisService: PaisService) {}
 
   usuario = {
-    nombre: 'César',
-    apellido:'González',
-    correo:'cedgo1997@gmail.com'
-  }
+    nombre: "César",
+    apellido: "González",
+    correo: "cedgo1997@gmail.com",
+  };
 
-
+  paises:any[] = [];
 
   ngOnInit() {
+    this.paisService.getPaises().subscribe((paises) => {
+      this.paises = paises;
+    });
   }
 
-
   guardar(forma: NgForm) {
-    console.log(forma)
+    console.log(forma);
 
-
-    if(forma.invalid) {
-      Object.values( forma.controls ).forEach( control => {
+    if (forma.invalid) {
+      Object.values(forma.controls).forEach((control) => {
         control.markAsTouched();
-      })
+      });
     }
 
-
-
-    console.log(forma.value)
+    console.log(forma.value);
   }
 }
